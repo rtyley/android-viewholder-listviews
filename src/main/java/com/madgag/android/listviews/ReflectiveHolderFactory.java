e7357b766a5faadcd2d21568d50f0356e2a6dc99
Kevin Sawicki <kevinsawicki@gmail.com>
2011-12-29 13:51:26 -0800

    Finalize parameters and correct typos in comments

--- src/main/java/com/madgag/android/listviews/ReflectiveHolderFactory.java
+++ src/main/java/com/madgag/android/listviews/ReflectiveHolderFactory.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2011 Kevin Sawicki <kevinsawicki@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 package com.madgag.android.listviews;
 
 import android.view.View;
@@ -8,14 +29,16 @@ import java.lang.reflect.Modifier;
 
 /**
  * {@link ViewHolderFactory} that uses reflection to create new
- * {@link ViewHolder} isntances with optional parameters
+ * {@link ViewHolder} instances with the given parameters that are injected into
+ * each {@link ViewHolder} created.
  * 
  * @param <T>
  *            model class that views are bound to
  */
 public class ReflectiveHolderFactory<T> implements ViewHolderFactory<T> {
 
-	private static Constructor<?> findMatch(Class<?> clazz, Class<?>[] types) {
+	private static Constructor<?> findMatch(final Class<?> clazz,
+			final Class<?>[] types) {
 		Constructor<?>[] candidates = clazz.getConstructors();
 		for (Constructor<?> candidate : candidates) {
 			Class<?>[] params = candidate.getParameterTypes();
@@ -45,7 +68,7 @@ public class ReflectiveHolderFactory<T> implements ViewHolderFactory<T> {
 	 * @throws RuntimeException
 	 */
 	@SuppressWarnings("unchecked")
-	public ReflectiveHolderFactory(Class<? extends ViewHolder<T>> holder,
+	public ReflectiveHolderFactory(final Class<? extends ViewHolder<T>> holder,
 			Object... args) {
 		if (args == null)
 			args = new Object[0];
@@ -69,7 +92,7 @@ public class ReflectiveHolderFactory<T> implements ViewHolderFactory<T> {
 		System.arraycopy(args, 0, this.args, 1, args.length);
 	}
 
-	public ViewHolder<T> createViewHolderFor(View view) {
+	public ViewHolder<T> createViewHolderFor(final View view) {
 		try {
 			args[0] = view;
 			return constructor.newInstance(args);
